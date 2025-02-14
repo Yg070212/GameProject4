6b6e7a6e0a14eb912207f621369c313f8051b993
Yg070212 <winter070212@gmail.com>
2025-02-14 19:44:46 +0900

    2025-02-14 <양방향 연결 리스트>

---
+++ Program/Program/Program.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* head;
+public:
+
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void push_front(T data)
+    {
+        Node* newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            newNode->data = data;
+            newNode->next = head;
+
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->next = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+
+        size++;
+    }
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            head = deleteNode->next;
+
+            delete deleteNode;
+
+            size--;
+        }
+    }
+
+    void show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " "
+
+            currentNode = currentNode->next;
+        }
+    }
+
+    void pop_back()
+    {
+
+
+        size--;
+    }
+
+};
+
+int main()
+{
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.push_front(10);
+    singleLinkedList.push_front(20);
+    singleLinkedList.push_back(5);
+    singleLinkedList.push_back(10);
+
+    singleLinkedList.pop_front();
+    singleLinkedList.pop_front();
+    singleLinkedList.pop_front();
+    singleLinkedList.pop_front();
+    singleLinkedList.pop_front();
+
+    singleLinkedList.show();
+
+
+    return 0;
+}
