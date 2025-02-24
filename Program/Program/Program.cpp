98a2d993e055d63ee73e7378a8044157f62b339d
Yg070212 <winter070212@gmail.com>
2025-02-24 19:44:47 +0900

    2025-02-24 <해시 테이블>

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -5,91 +5,30 @@
 using namespace std;
 
 template<typename T>
-class Stack
+class CircleQueue
 {
 private:
-    int top;
-    int size;
+    int rear;
+    int front;
+
     T container[SIZE];
 
 public:
-    Stack()
+    CircleQueue()
     {
-        top = 1;
-        size = 0;
+        rear = SIZE - 1;
+        front = SIZE - 1;
 
         for (int i = 0; i < SIZE; i++)
         {
             container[i] = NULL;
         }
     }
-
-    void push(T data)
-    {
-        if (top >= SIZE - 1)
-        {
-            cout << "Stack Overflow" << endl;
-        }
-        else
-        {
-            container[++top] = data; 
-        }
-    }
-
-    void pop()
-    {
-        if (Empty())
-        {
-            cout << "Stack is Empty" << endl;
-        }
-        else
-        {
-            top--;
-        }
-    }
-
-    bool Empty()
-    {
-        if (top <= -1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    const int& Size()
-    {
-        size = top + 1;
-
-        return size;
-    }
-
-    const T& Top()
-    {
-        return container[top];
-    }
-
 };
 
 int main()
 {
-    Stack<int> stack;
 
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    stack.push(40);
-    stack.push(50);
-
-    while (stack.Empty() == false)
-    {
-        cout << stack.Top() << endl;
-
-        stack.pop();
-    }
 
     return 0;
 }
