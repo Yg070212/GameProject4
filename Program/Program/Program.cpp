03828678859a1203ef316ac1855a8c21cb660457
Yg070212 <winter070212@gmail.com>
2025-03-04 19:43:59 +0900

    2025-03-04 <이진 탐색 트리>

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -23,6 +23,7 @@ private:
     };
 
     Bucket bucket[SIZE];
+
 public:
     HashTable()
     {
@@ -55,11 +56,75 @@ public:
         return hashIndex;
     }
 
+    Node* CreateNode(KEY key, VALUE value)
+    {
+        Node* newNode = new Node;
+
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        return newNode;
+    }
+
+    void Insert(KEY key, VALUE value)
+    {
+        // 해시 함수를 통해서 값을 받는 임시 변수.
+        int hashIndex = HashFunction(key);
+
+        // 새로운 노드를 생성합니다.
+        Node* newNode = CreateNode(key, value);
+
+        // 노드가 1개라도 존재하지 않는다면,
+        if (bucket[hashIndex].count == 0)
+        {
+            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        // bucket[hashIndex]의 count를 증가시킵니다.
+        bucket[hashIndex].count++;
+    }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            Node* deleteNode = bucket[i].head;
+            Node* nextNode = bucket[i].head;
+
+            if (bucket[i].head == nullptr)
+            {
+                continue;
+            }
+            else
+            {
+                while (nextNode != nullptr)
+                {
+                    nextNode = deleteNode->next;
+
+                    delete deleteNode;
+
+                    deleteNode = nextNode;
+                }
+            }
+        }
+    }
+
 };
 
 int main()
 {
+    HashTable<const char*, int> hashTable;
 
+    hashTable.Insert("Sword", 10000);
+    hashTable.Insert("Armor", 5000);
 
     return 0;
 }
